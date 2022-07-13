ec34df95fcb688d03d391c66a2f6e0e0215677d9
Romesh Singhabahu <romesh@earltech.it>
2022-07-13 21:38:55 +0200

    Initial Commit

---
+++ Telar/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  Telar
+//
+//  Created by Romesh Singhabahu on 13/07/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Telar/TelarApp.swift
@@ -0,0 +1,17 @@
+//
+//  TelarApp.swift
+//  Telar
+//
+//  Created by Romesh Singhabahu on 13/07/22.
+//
+
+import SwiftUI
+
+@main
+struct TelarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
